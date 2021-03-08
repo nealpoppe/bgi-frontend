47e532150c1196a6b3cbfae1bec225654b357439
nealpoppe <npoppe@gmail.com>
2021-03-08 15:39:54 -0600

    setting up initial pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Home"
+import Games from "./Games"
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      games: [],
+    }
+  }
+
+  componentDidMount = () => {
+    this.getGames();
+  }
+
+  getGames = async () => {
+    const response = await axios.get("https://api.boardgameatlas.com/api/search?&client_id=yXiWVjYNrj")
+    this.setState({
+      games: response.data.games
+    })
+  }
+
+  render() {
+    console.log(this.state.games)
+    return (
+      <div className="App">
+        <nav>
+          <Link exact to="/">Home</Link>
+          <Link exact to="/games">Games</Link>
+        </nav>
+        <div className="main">
+          <Route exact path="/" render={() => (
+            <Home />
+          )} />
+          <Route exact path="/games" render={() => (
+            <Games />
+          )} />
+        </div>
+        <h1>Welcome to Board Game Index</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Games.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Games = props => {
+  return(
+    <h1>Games</h1>
+  )
+}
+
+export default Games;
