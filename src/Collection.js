ffa3e90c829969b02e891be817688843929578c4
nealpoppe <npoppe@gmail.com>
2021-03-11 16:31:34 -0600

    sorting collection by name

--- src/Collection.js
+++ src/Collection.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Collection = props => {
 
+  //sorting the games by name
+  const myGames = props.addedGames;
+  myGames.sort((a,b) => (a.title > b.title) ? 1 : -1);
+
   return(
     <div>
       {props.addedGames.map(game =>
